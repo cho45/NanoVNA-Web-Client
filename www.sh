f6b46aae2d84773389ff2b15f2149a78dbcadde2
cho45 <cho45@lowreal.net>
2019-10-11 09:16:27 +0900

    optimize resources

--- www.sh
+++ www.sh
@@ -8,13 +8,19 @@ rm -rf www
 mkdir www
 ln -sf $PWD/*.js www
 ln -sf $PWD/*.html www
-ln -sf $PWD/images www
-ln -sf $PWD/dsp-wasm www
-cp -RL $PWD/lib www/lib
+cp -RL $PWD/dsp-wasm www
+cp -RL $PWD/lib www
 
 find www -print
 
 rm -r www/lib/webdfu
+rm -r www/dsp-wasm/src
+rm www/dsp-wasm/*.toml
+rm www/dsp-wasm/*.lock
+rm www/dsp-wasm/.gitignore
+rm www/dsp-wasm/no-modules/*.d.ts
+rm www/dsp-wasm/no-modules/package.json
+rm www/dsp-wasm/Makefile
 rm www/dfu.js
 rm www/dfu.html
 rm www/lib/comlink/*/node-adapter.*
